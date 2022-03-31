d20ec162b39f07242210c7c96760f1dbcdb7bfec
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2022-03-31 22:02:05 +0300

    First step

---
+++ DnDHelper/DnDHelper/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ConsoleApp
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
