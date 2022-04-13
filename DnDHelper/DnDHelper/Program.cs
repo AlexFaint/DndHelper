ad365b663ad8e5748f8b5afd288bbff87e3c0286
AlexFaint <60287281+AlexFaint@users.noreply.github.com>
2022-04-13 19:08:39 +0300

    tr1

--- DnDHelper/DnDHelper/Program.cs
+++ DnDHelper/DnDHelper/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ConsoleApp
 {
     public class Program
     {
+        public int DigitalRoot(long n)
+        {
+            // Your awesome code here!
+        }
+
         private static void Main()
         {
-            Console.WriteLine("Hello world");
+            int n;
+            int num;
+            n = 15;
+            if (n > 10)
+            {
+               
+            }
+            else num = n;
+
+            Console.WriteLine($"less then 10 sum={n}");
         }
     }
+
+    
 }
